5d168583789ca9f8cc6ab4b4e7c2c2fa7247953a
Lee Huskisson <leehuskisson@Lees-MacBook-Pro.local>
2022-05-23 14:06:34 -0700

    initial commit

---
+++ migration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE recipe(
+    recipe_id SERIAL PRIMARY KEY,
+    ingredients TEXT,
+    instructions TEXT
+);
+
+
+
---
+++ seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO recipe(title, ingredients, instructions)VALUES('PB&J', 'Peanut Butter. Jelly. Bread.', 'Spread peanut butter on bread. Spread jelly on bread. Put jelly side and peanut butter side together. Munch');
+
