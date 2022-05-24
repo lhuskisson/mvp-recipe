9be6eb20a815bbab804b84e71b696be8db31324e
Lee Huskisson <leehuskisson@Lees-MacBook-Pro.local>
2022-05-23 18:06:34 -0700

    update migration

--- migration.sql
+++ migration.sql
@@ -1,7 +1,12 @@
+DROP DATABASE IF EXISTS recipe_db;
+
+CREATE DATABASE recipe_db;
+
+DROP TABLE IF EXISTS recipe;
 
 CREATE TABLE recipe(
-    recipe_id SERIAL PRIMARY KEY,
-    recipe_ingredients TEXT,
-    recipe_instructions TEXT
+   id SERIAL PRIMARY KEY NOT NULL,
+   recipe_name TEXT NOT NULL,
+   recipe_ingredients TEXT NOT NULL,
+   recipe_instructions TEXT NOT NULL,
 );
-
