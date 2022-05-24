801940928eb9bf4fbe2ec13c3495cd32ba3abaff
Lee Huskisson <leehuskisson@Lees-MacBook-Pro.local>
2022-05-24 08:09:01 -0700

    server file updated

--- migration.sql
+++ migration.sql
@@ -1,9 +1,4 @@
-DROP DATABASE IF EXISTS recipe_db;
-
 CREATE DATABASE recipe_db;
-
-DROP TABLE IF EXISTS recipe;
-
 CREATE TABLE recipe(
    id SERIAL PRIMARY KEY NOT NULL,
    recipe_name TEXT NOT NULL,
