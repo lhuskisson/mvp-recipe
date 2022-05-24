e694ddd6ceaa6c49b945d7d3f9d454514e66efb8
Lee Huskisson <leehuskisson@Lees-MacBook-Pro.local>
2022-05-24 08:42:08 -0700

    update migration and seed

--- migration.sql
+++ migration.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE recipe_db;
-CREATE TABLE recipe(
+
+CREATE TABLE recipe (
    id SERIAL PRIMARY KEY NOT NULL,
    recipe_name TEXT NOT NULL,
    recipe_ingredients TEXT NOT NULL,
-   recipe_instructions TEXT NOT NULL,
+   recipe_instructions TEXT NOT NULL
 );
