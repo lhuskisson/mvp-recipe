21d35b834449d4644f2c7922cb2d81394f79f3c8
Lee Huskisson <leehuskisson@Lees-MacBook-Pro.local>
2022-05-27 10:36:31 -0700

    update

--- pub/main.js
+++ pub/main.js
@@ -4,13 +4,49 @@ const btn = document.querySelector('#btn')
 addListeners()
 
 function addListeners() {
-    btn.addEventListener('click', getRecipe)
+    window.addEventListener('load', getRecipe)
 }
 
 async function getRecipe() {
     const result = await fetch('http://localhost:8000/api/recipe')
     const data = await result.json()
-    createRecipes(data)
+    console.log(data)
+    createRecipe(data)
+    
 }
 
+function createRecipe (arr) {
+    arr.forEach((elem) => {
+        createOneRecipe(elem)
+    })
+}
 
+function createOneRecipe(elem) {
+    const div = document.createElement('div')
+    const div2 = document.createElement('div')
+    const div3 = document.createElement('div')
+    div.textContent = elem.recipe_name
+    div2.textContent = elem.recipe_ingredients
+    div3.textContent = elem.recipe_instructions
+    div.className = 'listItem'
+    div.id = elem.id
+    
+    addEventListenerToOneRecipe(div)
+    appendListItemToList(div)
+    appendListItemToList(div2)
+    appendListItemToList(div3)
+}
+
+function appendListItemToList(div) {
+    var list = document.querySelector('#list')
+    list.append(div)
+}
+function addEventListenerToOneRecipe(div) {
+    div.addEventListener('click', getOneRecipe)
+}
+
+async function getOneRecipe(e) {
+    const result = await fetch (`http://localhost:8000/api/recipe/${e.target.id}`)
+    const data = await result.json()
+    console.log(data)
+}
