2c86e7b7237502c0a662f8bbd2100bcf58c9f458
Lee Huskisson <leehuskisson@Lees-MacBook-Pro.local>
2022-05-30 13:49:54 -0700

    sdsd

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const {Pool} = require('pg')
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthoried: false
+        rejectUnauthorized: false
     }
 });
 module.exports = pool;
